cda174e1e38d28ec0e30c5ac6902d2cd144b4897
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-05 19:21:14 +0500

    express app setup, database connection

---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbconnect = require('./src/db/dbconnect');
+const app = require('./app');
+const PORT = process.env.PORT || 8000;
+
+dbconnect()
+    .then(() => {
+        app.listen(PORT, () => { console.log(`server running on Port ${PORT}`) });
+    })
+    .catch((error) => {
+        console.log(`Mongodb connection failed: ${error}`);
+    });
+
