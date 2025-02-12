b41df1fc2ca53d0c1b30a5b52fcc5b0c886901f5
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-12 13:23:32 +0500

    login admin complete, working on getAllUsers

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,8 +1,59 @@
-const getAllUsers = (req, res) => {
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const { validateLoginUser } = require('../validations/user.validations');
 
+const loginAdmin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-} 
+        const { error } = validateLoginUser({email, password});
+        if (error) return res.status(401).json({"error": error.details[0].message });
+
+        const user = await User.findOne({ email });
+        if(!user) return res.status(404).json({"error": "Invalid email"});
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if(!validPassword) return res.status(401).json({"error": "Invalid password"});
+
+        if(!user.isAdmin) return res.status(401).json({"error": "Access denied. Only admins are allowed..."});
+
+        const token = user.generateAuthToken();
+        return res.status(200).json({ token });
+
+    } catch (error) {
+        const result = {
+            "error-code": error.code ? error.code : "no error code",
+            "erro-message": error.message ? error.message : "Internal server error"
+        };
+        return res.status(500).json(result);
+    }
+}
+
+const getAllUsers = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.user.userId });
+        // if(!user.isAdmin) return res.status(401).json({"error":"unauthorized access"});
+
+        const users = await User.find();
+        // const {password, resetPasswordExpires, resetPasswordToken, ...restData} = users;
+        let result = {};
+        users.forEach(user => {
+            const { password, resetPasswordExpires, resetPasswordToken, ...restUser } = user
+            result.push(restUser);
+        });
+        return res.status(200).json(result);
+
+    } catch (error) {
+        const result = {
+            "error-code": error.code ? error.code : "no error code",
+            "erro-message": error.message ? error.message : "Internal server error"
+        };
+        return res.status(500).json(result);
+    }
+
+}
 
 module.exports = {
+    loginAdmin,
     getAllUsers
 }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,16 @@
+const {
+    loginAdmin,
+    getAllUsers
+} = require('../controllers/admin.controller');
+
+const auth = require('../middlewares/auth.middleware');
+
 const express = require('express');
 const router = express.Router();
 
+router.post('/login-admin', loginAdmin);
 
+//protected routes
+router.get('/get-all-users', [auth], getAllUsers);
 
 module.exports = router;
