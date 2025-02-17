0fa9524850d74e83290e013b3e276b985355bcc5
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-17 13:33:36 +0500

    cloudinay.util function complete

--- src/utils/cloudinary.util.js
+++ src/utils/cloudinary.util.js
@@ -1,4 +1,5 @@
 const { v2 } = require("cloudinary");
+const fs = require('fs');
 
 v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,10 +7,15 @@ v2.config({
     api_secret: process.env.CLOUDINAY_API_SECRET
 });
 
-const uploadOnCloudinary = () => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        
+        if(!localFilePath) return null;
+        const upload = await cloudinay.uploader.upload(localFilePath, { resource_type: "auto"});
+        fs.unlink(localFilePath);
+        return upload;
     } catch (error) {
+        fs.unlink(localFilePath);
+        console.error(error);
         return null;
     }
 }
