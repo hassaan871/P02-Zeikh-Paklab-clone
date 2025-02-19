b380c2f9658de25cb624cd7264c748d49fcfbe07
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-19 18:28:49 +0500

    delete from cart api boiler plate

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -48,6 +48,18 @@ const addToCartController = async (req, res) => {
     }
 }
 
+const deleteFromCartController = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        const result = {
+            "error-code": error.code ? error.code : "no error code",
+            "error-message": error.message ? error.message : "Internal server error"
+        }
+        return res.status(500).json(result);
+    }
+}
 module.exports = {
-    addToCartController
+    addToCartController,
+    deleteFromCartController
 }
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,4 +1,4 @@
-const { addToCartController } = require('../controllers/cart.controller');
+const { addToCartController, deleteFromCartController } = require('../controllers/cart.controller');
 
 const auth = require('../middlewares/auth.middleware');
 
@@ -6,5 +6,6 @@ const express = require('express');
 const router = express.Router();
 
 router.put('/add-to-cart', [auth], addToCartController);
+router.put('/delete-from-cart', [auth], deleteFromCartController);
 
 module.exports = router;
