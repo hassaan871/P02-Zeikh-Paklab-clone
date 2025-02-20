349e85478230a583d17b9f789644ea7dd3c81a7d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-20 13:36:37 +0500

    order model v1

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        required: true,
+        enum: ["Paid","COD"]
+    },
+    orderStatus: {
+        type: String,
+        required: true,
+        enum: ["processing", "pending", "shipped", "delivered", "canceled"]
+    }
+
 
 }, {timestamps: true});
 
