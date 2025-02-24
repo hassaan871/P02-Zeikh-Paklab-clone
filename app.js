5531fb8ab3459a2d0d62bb5161f34f4da6ee84d7
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-24 12:04:32 +0500

    views/index.ejs to test sockets.io

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname,'views')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
