5ec6e1883694c7271ae739964ca0a9ae8bb82008
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-24 12:18:23 +0500

    server configured to use socket.io

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const http = require('http');
 const dbconnect = require('./src/db/dbconnect');
 const app = require('./app');
+const setupSocket = require('./src/sockets/socket');
 const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+setupSocket(server);
 
 dbconnect()
     .then(() => {
-        app.listen(PORT, () => { console.log(`server running on Port ${PORT}`) });
+        server.listen(PORT, () => { console.log(`server running on Port ${PORT}`) });
     })
     .catch((error) => {
         console.log(`Mongodb connection failed: ${error}`);
---
+++ src/sockets/socket.js
@@ -0,0 +1,7 @@
+
+
+const setupSocket = () => {
+
+}
+
+module.exports = setupSocket;
