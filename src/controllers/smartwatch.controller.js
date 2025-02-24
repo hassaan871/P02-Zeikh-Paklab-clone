a3bc7ddb6fa6874cfbd05021e3ad708d59f354d0
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-24 17:22:15 +0500

    smartwatch controller updated

--- src/controllers/smartwatch.controller.js
+++ src/controllers/smartwatch.controller.js
@@ -1,8 +1,9 @@
 const Smartwatch = require('../models/smartwatch.model');
 const { uploadOnCloudinary } = require('../utils/cloudinary.util');
+const asyncHandler = require('../utils/asyncHandler');
 
 const addSmartWatchProductController = async (req, res) => {
-    try {
+
         const {
             name,
             description,
@@ -47,18 +48,10 @@ const addSmartWatchProductController = async (req, res) => {
         });
 
         return res.status(201).json(smartwatch);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const addSmartWatchImageController = async (req, res) => {
-    try {
+
         if (!req.file) return res.status(400).json({ "error": "file not provided to be uplaoded" });
         if (!req.body.smartwatchId) return res.status(400).json({ "error": "smartwatch id not provided" });
 
@@ -67,32 +60,18 @@ const addSmartWatchImageController = async (req, res) => {
         if (!smartwatch) return res.status(404).json({ "error": "smartwatch not found" });
 
         return res.status(200).json({ "success": "Image uploaded successfully", smartwatch });
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllSmartWatchesController = async (req, res) => {
-    try {
+
         const smartwatches = await Smartwatch.find();
         if(!smartwatches) return res.status(404).json(smartwatches);
 
         return res.status(200).json(smartwatches);
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const searchSmartwatchController = async (req, res) => {
-    try {
+
         const { name } = req.body;
         if (!name) return res.status(400).json({ "error": "must required smartwatch name to be searched" });
         
@@ -100,35 +79,20 @@ const searchSmartwatchController = async (req, res) => {
         if(!smartwatch) return res.status(404).json({"error": "no smartwatch found"});
 
         return res.status(200).json(smartwatch);
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const softDeleteSmartwatchController = async (req, res) => {
-    try {
+
         const smartwatch = await Smartwatch.findOneAndUpdate({"_id": req.body.smartwatchId}, { $set: {isDeleted: true}},{new: true});
         if(!smartwatch) return res.status(404).json({"error": "Smartwatch not found. Invalid smartwatchId"});
 
         return res.status(200).json({"success": "Smartwatch softly deleted", smartwatch});
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 module.exports = {
-    addSmartWatchProductController,
-    addSmartWatchImageController,
-    getAllSmartWatchesController,
-    searchSmartwatchController,
-    softDeleteSmartwatchController
+    addSmartWatchProductController: asyncHandler(addSmartWatchProductController),
+    addSmartWatchImageController: asyncHandler(addSmartWatchImageController),
+    getAllSmartWatchesController: asyncHandler(getAllSmartWatchesController),
+    searchSmartwatchController: asyncHandler(searchSmartwatchController),
+    softDeleteSmartwatchController: asyncHandler(softDeleteSmartwatchController)
 }
