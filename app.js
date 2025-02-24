d49c50f078efc71438c9ba1f4c4e213441b12df5
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-24 12:38:27 +0500

    socket.io configured and set to check how much users are active and using platform

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'views')));
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(express.static(path.join(__dirname, 'src','views')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
 
 app.use('/api',routes);
 
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,7 +1,22 @@
+const socketIO = require('socket.io');
 
+const setupSocket = (server) => {
+    const io = socketIO(server);
 
-const setupSocket = () => {
+    let count = 0;
+    io.on('connection', (socket)=>{
+        console.log("a user connected...");
 
+        count++;
+        socket.emit('broadcast', {message: "hey, welcome!!!"});
+        socket.broadcast.emit('broadcast', {message: `${count} users connected`});
+
+        socket.on('disconnet', ()=>{
+            console.log("a user disconnected...");
+            count--;
+            io.sockets.emit('broadcast', ()=> {message: `${count} user's connected`});
+        });
+    });
 }
 
 module.exports = setupSocket;
