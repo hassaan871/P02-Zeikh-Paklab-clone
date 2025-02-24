9fa824ebcdb8e390e0e1221793e1c17b17d11091
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-24 16:59:11 +0500

    admin controllers updated

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,6 +1,9 @@
 const User = require('../models/user.model');
 const Order = require('../models/order.model');
+const asyncHandler = require('../utils/asyncHandler');
+
 const bcrypt = require('bcrypt');
+
 const { validateLoginUser } = require('../validations/user.validations');
 const {
     addLaptopProductController,
@@ -14,71 +17,56 @@ const {
 } = require('./smartwatch.controller');
 
 const loginAdminController = async (req, res) => {
-    try {
+
         const { email, password } = req.body;
 
         const { error } = validateLoginUser({ email, password });
-        if (error) return res.status(401).json({ "error": error.details[0].message });
+        if (error) return res.status(401).json({ "error-message": error.details[0].message });
 
         const user = await User.findOne({ email });
-        if (!user) return res.status(404).json({ "error": "Invalid email" });
+        if (!user) return res.status(404).json({ "error-message": "Invalid email" });
 
         const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) return res.status(401).json({ "error": "Invalid password" });
+        if (!validPassword) return res.status(401).json({ "error-message": "Invalid password" });
 
-        if (!user.isAdmin) return res.status(401).json({ "error": "Access denied. Only admins are allowed..." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Access denied. Only admins are allowed..." });
 
         const token = user.generateAuthToken();
         return res.status(200).json({ token });
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "erro-message": error.message ? error.message : "Internal server error"
-        };
-        return res.status(500).json(result);
-    }
 }
 
 const makeAdminController = async (req, res) => {
-    try {
-        if (!req.body.email && !req.body.userId) return res.status(400).json({ "error": "Email or userId is required to perform the action" });
+
+        if (!req.body.email && !req.body.userId) return res.status(400).json({ "error-message": "Email or userId is required to perform the action" });
 
         if (req.body.email) {
             const user = await User.findOne({ email: req.body.email });
-            if (!user) return res.status(404).json({ "error": "User not found" });
+            if (!user) return res.status(404).json({ "error-message": "User not found" });
 
             if (user.superAdmin) return res.status(401).json({ "ERROR": "You cannot edit or view SUPER ADMIN" });
 
             user.isAdmin = true;
             await user.save();
 
-            return res.status(200).json({ "success": `${user.email} is now admin` });
+            return res.status(200).json({ "success-message": `${user.email} is now admin` });
         }
         if (req.body.userId) {
             const user = await User.findOne({ _id: req.body.userId });
-            if (!user) return res.status(404).json({ "error": "User not found" });
+            if (!user) return res.status(404).json({ "error-message": "User not found" });
 
             if (user.superAdmin) return res.status(401).json({ "ERROR": "You cannot edit or view SUPER ADMIN" });
 
             user.isAdmin = true;
             await user.save();
 
-            return res.status(200).json({ "success": `${user.email} is now admin` });
+            return res.status(200).json({ "success-message": `${user.email} is now admin` });
         }
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        };
-        return res.status(500).json(result);
-    }
 }
 
 const getAllUsersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const users = await User.find();
         const result = [];
@@ -89,265 +77,153 @@ const getAllUsersController = async (req, res) => {
         console.log(result);
 
         return res.status(200).json(result);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        };
-        return res.status(500).json(result);
-    }
 }
 
 const createLaptopProductController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         await addLaptopProductController(req, res);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const uploadLaptopImageController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         await addLaptopImageController(req, res);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const createSmartWatchProductController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         await addSmartWatchProductController(req, res);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const uploadSmartWatchImageController = async (req, res) => {
-    try {
+    
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         await addSmartWatchImageController(req, res);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const deleteLaptopController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access. " });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access. " });
 
         await softDeleteLaptopController(req, res);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const deleteSmartwatchController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access. " });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         await softDeleteSmartwatchController(req, res);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllOrdersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const allOrders = await Order.find();
-        if(!allOrders) return res.status(404).json({"error": "not a single order"});
+        if(!allOrders) return res.status(404).json({"error-message": "not a single order found"});
 
         return res.status(200).json(allOrders);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllCanceledOrdersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const canceledOrders = await Order.find({orderStatus: "canceled"});
-        if(!canceledOrders) return res.status(404).json({"error":"not a single Canceled order"});
+        if(!canceledOrders) return res.status(404).json({"error-message":"not a single Canceled order"});
 
         return res.status(200).json(canceledOrders);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllDeliveredOrdersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const deliveredOrders = await Order.find({orderStatus: "delivered"});
-        if(!deliveredOrders) return res.status(404).json({"error":"not a single order Delivered"});
+        if(!deliveredOrders) return res.status(404).json({"error-message":"not a single order Delivered"});
 
         return res.status(200).json(deliveredOrders);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllShippedOrdersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const shippedOrders = await Order.find({orderStatus: "shipped"});
-        if(!shippedOrders) return res.status(404).json({"error":"not a single order shipped"});
+        if(!shippedOrders) return res.status(404).json({"error-message":"not a single order shipped"});
 
         return res.status(200).json(shippedOrders);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllPendingOrdersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const pendingOrders = await Order.find({orderStatus: "pending"});
-        if(!pendingOrders) return res.status(404).json({"error":"not a single pending order"});
+        if(!pendingOrders) return res.status(404).json({"error-message":"not a single pending order"});
 
         return res.status(200).json(pendingOrders);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const getAllProcessingOrdersController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const processingOrders = await Order.find({orderStatus: "processing"});
-        if(!processingOrders) return res.status(404).json({"error":"not a single order processing"});
+        if(!processingOrders) return res.status(404).json({"error-message":"not a single order processing"});
 
         return res.status(200).json(processingOrders);
-
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
 }
 
 const updateOrderStatusController = async (req, res) => {
-    try {
+
         const user = await User.findOne({ _id: req.user.userId });
-        if (!user.isAdmin) return res.status(401).json({ "error": "Unauthorized Access." });
+        if (!user.isAdmin) return res.status(401).json({ "error-message": "Unauthorized Access." });
 
         const {orderId, orderStatus} = req.body;
-        if((!orderId || !orderStatus)) return res.status(400).json({"error": "orderId and orderStatus required!"});
+        if((!orderId || !orderStatus)) return res.status(400).json({"error-message": "orderId and orderStatus required!"});
 
         const order = await Order.findOne({_id: orderId});
         order.orderStatus = orderStatus;
         await order.save();
 
-        return res.status(200).json({"success": `orderStatus updated to ${orderStatus}`, order});
-        
-    } catch (error) {
-        const result = {
-            "error-code": error.code ? error.code : "no error code",
-            "error-message": error.message ? error.message : "Internal server error"
-        }
-        return res.status(500).json(result);
-    }
+        return res.status(200).json({"success-message": `orderStatus updated to ${orderStatus}`, order});
 }
 
 
 module.exports = {
-    loginAdminController,
-    makeAdminController,
-    getAllUsersController,
-    createLaptopProductController,
-    uploadLaptopImageController,
-    createSmartWatchProductController,
-    uploadSmartWatchImageController,
-    deleteLaptopController,
-    deleteSmartwatchController,
-    getAllOrdersController,
-    getAllCanceledOrdersController,
-    getAllDeliveredOrdersController,
-    getAllShippedOrdersController,
-    getAllPendingOrdersController,
-    getAllProcessingOrdersController,
-    updateOrderStatusController
+    loginAdminController: asyncHandler(loginAdminController),
+    makeAdminController: asyncHandler(makeAdminController),
+    getAllUsersController: asyncHandler(getAllUsersController),
+    createLaptopProductController: asyncHandler(createLaptopProductController),
+    uploadLaptopImageController: asyncHandler(uploadLaptopImageController),
+    createSmartWatchProductController: asyncHandler(createSmartWatchProductController),
+    uploadSmartWatchImageController: asyncHandler(uploadSmartWatchImageController),
+    deleteLaptopController: asyncHandler(deleteLaptopController),
+    deleteSmartwatchController: asyncHandler(deleteSmartwatchController),
+    getAllOrdersController: asyncHandler(getAllOrdersController),
+    getAllCanceledOrdersController: asyncHandler(getAllCanceledOrdersController),
+    getAllDeliveredOrdersController: asyncHandler(getAllDeliveredOrdersController),
+    getAllShippedOrdersController: asyncHandler(getAllShippedOrdersController),
+    getAllPendingOrdersController: asyncHandler(getAllPendingOrdersController),
+    getAllProcessingOrdersController: asyncHandler(getAllProcessingOrdersController),
+    updateOrderStatusController: asyncHandler(updateOrderStatusController)
 }
