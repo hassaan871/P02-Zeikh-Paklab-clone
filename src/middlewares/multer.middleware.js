63de044f716113e9888735fe08e7015bfa48b3a0
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-24 14:27:01 +0500

    code refactor: moved public/temp out of the src

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './src/public/temp');
+        cb(null, './public/temp');
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
