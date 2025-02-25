2671234ca4710156343b0d56b37f327678ef8b49
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-25 15:53:27 +0500

    updated http methods of add to cart and delete from cart api

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -5,8 +5,8 @@ const auth = require('../middlewares/auth.middleware');
 const express = require('express');
 const router = express.Router();
 
-router.put('/add-to-cart', [auth], addToCartController);
-router.put('/delete-from-cart', [auth], deleteFromCartController);
+router.post('/add-to-cart', [auth], addToCartController);
+router.delete('/delete-from-cart', [auth], deleteFromCartController);
 router.get('/get-cart', [auth], getCartController);
 
 module.exports = router;
