daa24c5e91dba2e939ddad52a57ea11950bae5dc
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-25 18:34:33 +0500

    add review validation complete

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,13 +1,14 @@
 const Review = require('../models/review.model');
+const Order = require('../models/order.model');
 
 const asyncHandler = require('../utils/asyncHandler');
 
 const addReviewController = async (req, res) => {
 
         const { laptopId, smartwatchId, value, quality, price, review } = req.body;
-        if (!laptopId && !smartwatchId) return res.status(400).json({ "error": "product Id is required. (smartwatchId or laptopId)" });
+        if (!laptopId && !smartwatchId) return res.status(400).json({ "error-message": "product Id is required. (smartwatchId or laptopId)" });
 
-        if (!value && !quality && !price && !review) return res.status(400).json({ "error": "Reviews details is required." });
+        if (!value && !quality && !price && !review) return res.status(400).json({ "error-message": "Reviews details is required." });
 
         const { userId } = req.user;
         const reviewDocument = {
@@ -19,34 +20,62 @@ const addReviewController = async (req, res) => {
         };
 
         if (laptopId) {
+            const order = await Order.findOne({
+                userId,
+                product:{
+                    $elemMatch:{
+                        itemId: laptopId
+                    }
+                }    
+            });
+            if(!order || order.orderStatus !== "delivered") return res.status(400).json(
+                {
+                    "error-message": "can't add a review to the product not delivered to you"
+                }
+            );
             reviewDocument.productId = laptopId;
             reviewDocument.category = "Laptop";
         }
         if (smartwatchId) {
+            const order = await Order.findOne({
+                userId,
+                product:{
+                    $elemMatch:{
+                        itemId: smartwatchId
+                    }
+                }    
+            });
+            if(!order || order.orderStatus !== "delivered") return res.status(400).json(
+                {
+                    "error-message": "can't add a review to the product not delivered to you"
+                }
+            );
             reviewDocument.productId = smartwatchId;
             reviewDocument.category = "Smartwatch";
         }
 
         const userReview = await Review.create(reviewDocument);
 
-        return res.status(201).json({ "success": "review posted successfully", userReview });
+        return res.status(201).json({ "success-messsage": "review posted successfully", userReview });
 }
 
 const updateReviewController = async (req, res) => {
     
         const { productId, value, quality, price, updatedReview } = req.body;
-        if (!productId) return res.status(400).json({ "error": "productId is required" });
+        if (!productId) return res.status(400).json({ "error-message": "productId is required" });
 
         if (!value && !quality && !price && !review) return res.json(400).json({ "error": "atleast one figure to be updated required" });
 
         const review = await Review.findOne({ userId, productId });
+        if(!review) return res.status(400).json({"error-message": "no reivew to update"});
+
         if (value) review.value = value;
         if(quality) review.quality = quality;
         if(price) review.price = price;
         if(updatedReview) review.review = updatedReview;
 
         await review.save();
-        return res.status(200).json({"success": "review updated succsessfully", review});
+        return res.status(200).json({"success-message": "review updated succsessfully", review});
 }
 
 const deleteReviewController = async (req, res) => {
@@ -54,17 +83,17 @@ const deleteReviewController = async (req, res) => {
         const { userId } = req.user;
 
         const { productId } = req.body;
-        if (!productId) return res.status(400).json({ "error": "productId is required." });
+        if (!productId) return res.status(400).json({ "error-message": "productId is required." });
 
         const review = await Review.findOne({ userId, productId });
-        if (!review) return res.status(404).json({ "error": "review not found" });
+        if (!review) return res.status(404).json({ "error-message": "review not found" });
 
-        if (review.isDeleted) return res.status(400).json({ "error": "review already deleted" });
+        if (review.isDeleted) return res.status(400).json({ "error-message": "review already deleted" });
 
         review.isDeleted = true;
         await review.save();
 
-        return res.status(200).json({ "success": "review deleted successfully." });
+        return res.status(200).json({ "success-message": "review deleted successfully." });
 }
 
 module.exports = {
